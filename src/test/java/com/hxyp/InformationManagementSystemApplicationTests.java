8c43c4978781fc6ffc3adad7cfb3a22965d0210a
Peicaiyuan <2324930778@qq.com>
2020-08-24 22:45:31 +0800

    2020-8-24
    创建项目

---
+++ src/test/java/com/hxyp/InformationManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hxyp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InformationManagementSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
