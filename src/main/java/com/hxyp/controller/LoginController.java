e5c14ada946f6d765265acec57329827e9fa58b0
Peicaiyuan <2324930778@qq.com>
2020-08-27 15:36:08 +0800

    重新部署前端页面

--- src/main/java/com/hxyp/controller/LoginController.java
+++ src/main/java/com/hxyp/controller/LoginController.java
@@ -14,8 +14,8 @@ package com.hxyp.controller;
 import com.hxyp.entity.Login;
 import com.hxyp.service.ILoginService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 
 /**
@@ -26,14 +26,14 @@ import org.springframework.web.bind.annotation.RestController;
  * @create 2020/8/25
  * @since 1.0.0
  */
-@RestController
+@Controller
 public class LoginController {
     @Autowired
     private ILoginService iLoginService;
 
     @RequestMapping("/")
     public String index(){
-        return "index";
+        return "login";
     }
 
     //登录验证
@@ -42,11 +42,11 @@ public class LoginController {
         Integer result = iLoginService.identDiscrimination(username,password);
         //1为超级管理员，2为领导，3为教师
         if(result == 1){
-            return "admin/superAdmin";
+            return "index";
         } else if (result == 2){
-            return "leader";
+            return "index";
         } else if (result == 3){
-            return "teacher";
+            return "index";
         } else {
             return "redirect:/";//重新登录
         }
